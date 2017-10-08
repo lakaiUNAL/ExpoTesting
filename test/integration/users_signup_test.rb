52e3b0c628a0351c38be58a47513826d2cf97574
Juan Carlos <jcgamar@unal.edu.co>
2017-10-08 12:19:52 -0500

    Fin cap 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
   test "Invalid Sing Up Information" do
     #Assert 
     get singup_path
@@ -18,4 +19,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     #assert_select 'div.<CSS class for field with error>'
   end
+
+  test "valid signup information" do
+    
+    #get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+
+    assert_not flash.empty?
+
+    #assert_select ".success"
+    assert_select ".alert-success"
+
+  end
+
 end
